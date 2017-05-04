59ebe198c1acf4eced11f29b1befbcef58c6819b
Stefan <stefan@Lubomirs-MacBook-Pro.local>
2017-05-04 11:18:24 +0200

    before converting csv parser to swift 3

--- Pokedex/PokemonCell.swift
+++ Pokedex/PokemonCell.swift
@@ -9,5 +9,21 @@
 import UIKit
 
 class PokemonCell: UICollectionViewCell {
+    @IBOutlet weak var pokemonImage : UIImageView!
+    @IBOutlet weak var nameLbl : UILabel!
+    
+    var pokemon : Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0 
+    }
+    
+    func configureCell ( pokemon : Pokemon ) {
+        self.pokemon = pokemon
+        self.nameLbl.text = pokemon.name.capitalized
+        self.pokemonImage.image = UIImage(named: "\(pokemon.pokedexId)")
+    }
     
 }
