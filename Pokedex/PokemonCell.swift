87ef1cf4d539a61378e387b085df8a7ff19395df
Stefan <stefan@Lubomirs-MacBook-Pro.local>
2017-05-04 11:14:55 +0200

    before converting to swift 3

---
+++ Pokedex/PokemonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonCell.swift
+//  Pokedex
+//
+//  Created by Stefan on 04/05/17.
+//  Copyright © 2017 Synetech. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCell: UICollectionViewCell {
+    
+}
